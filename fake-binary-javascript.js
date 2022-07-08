024ac14a63ad06ddec2aed4a2872cca3d60c881e
50ShadesOfCode <kaktymail@gmail.com>
2022-07-09 01:19:13 +0300

    Update fake-binary-javascript.js

--- fake-binary-javascript.js
+++ fake-binary-javascript.js
@@ -1,7 +1,7 @@
 function fakeBin(string){
     ans = ''
     string.split('').map((c, i) => {
-        (parseint(c) < 5 ? ans += '0' : ans += '1')
+        (parseInt(c) < 5 ? ans += '0' : ans += '1')
     })
     return ans;
 }
